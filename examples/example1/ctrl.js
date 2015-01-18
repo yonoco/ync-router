a38c073dc9c1e68d3c9bf041a524a32b8ec4780b
tfitos <tamas.fitos@gmail.com>
2015-01-18 13:15:25 +0100

    parameters in different orders are working now

--- examples/example1/ctrl.js
+++ examples/example1/ctrl.js
@@ -9,8 +9,10 @@ module.exports = {
         console.log(this);
     },
 
-    world: function*() {
+    world: function*(param2, param3) {
         console.log("world called");
+        console.log("param2=" + param2);
+        console.log("param3=" + param3);
     }
 
 }
